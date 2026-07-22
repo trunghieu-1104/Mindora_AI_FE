6e037c31e30a3fc217f6613f1c4013cbbce8b52d
trunghieu-1104 <trunghieu20221107@gmail.com>
2026-07-22 09:24:04 +0700

    chore: connect frontend to Render production backend

--- Mindora_AI/src/lib/api.js
+++ Mindora_AI/src/lib/api.js
@@ -1,4 +1,4 @@
-const API_BASE = 'http://localhost:8080/api'
+const API_BASE = 'https://mindorabe.onrender.com/api'
 
 export const getToken = () => localStorage.getItem('mindora_token')
 export const setToken = (token) => localStorage.setItem('mindora_token', token)
